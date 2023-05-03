d273b24299fbdeb091bebd54f4373c82161fc944
Daly <dalyna64@gmail.com>
2023-05-03 22:20:06 +0100

    03/05/2023 optimize the code

---
+++ app/src/main/java/com/alphadaly/taskslist/application/Global.kt
@@ -0,0 +1,11 @@
+package com.alphadaly.taskslist.application
+
+import android.annotation.SuppressLint
+import android.content.Context
+
+@SuppressLint("StaticFieldLeak")
+object Global{
+    lateinit var context: Context
+
+
+}
